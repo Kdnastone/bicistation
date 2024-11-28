999d20fb439eaf3af21e7b9ca1a67c9d3bfba700
KdnaStone <72689531+Kdnastone@users.noreply.github.com>
2024-11-28 16:27:56 -0500

    Update ContainerCards.jsx

--- src/components/functionals/ContainerCards.jsx
+++ src/components/functionals/ContainerCards.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 function ContainerCards() {
-  return (
-    <div>ContainerCards</div>
-  )
-}
+  const [stations, setStations] = useState([]);
 
-export default ContainerCards
+  useEffect(() => {
+    fetch('http://api.citybik.es/v2/networks/velib')
+      .then((response) => response.json())
+      .then((data) => {
+        setStations(data.network ? data.network.stations : []);
+      })
+      .catch((error) => console.error('Error cargando la información:', error));
+  }, []);
+
+  return (
+    <div className="station-list">
+      {stations.length > 0 ? (
+        stations.map((station) => (
+          <div key={station.id} className="card">
+            <h3>{station.name}</h3>
+            <p><strong>Empresa:</strong> {station.company}</p>
+            <p><strong>Nombre de la Estación:</strong> {station.name}</p>
+            <p><strong>País:</strong> {station.location.city}</p>
+            <p><strong>Ciudad:</strong> {station.location.city}, {station.location.country}</p>
+            <p><strong>Bicicletas Disponibles:</strong> {station.free_bikes}</p>
+            <p><strong>Parqueaderos Disponibles:</strong> {station.empty_slots}</p>
+          </div>
+        ))
+      ) : (
+        <p>Cargando información ...</p>
+      )}
+    </div>
+  );
+}
