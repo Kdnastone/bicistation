f1ec71595c8560488370b0356bc30382ce33d456
Leslye <138824809+Les138@users.noreply.github.com>
2024-11-28 16:41:30 -0500

    Update App.jsx

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,29 +1,20 @@
-import { useState } from 'react';
-
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-
-import Parking from "../pages/Parking";
-import Rent from "../pages/Rent";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home, Parking, Rent, Error404} from "../pages/Ruteo";
 import Header from '../components/basics/Header';
+import Navbar from '../components/basics/Navbar';
 import Footer from '../components/basics/Footer';
 
 const App = () => {
   return (
     <Router>
-      <Header />
-      <div className="main-content">
-        <Routes>
-          <Route path="/Parking" element={<Parking />} />
-          <Route path="/Rent" element={<Rent />} />
-
-          {/* Redirigir a /Rent por defecto */}
-          <Route path="/" element={<Navigate to="/Rent" replace />} />
-
-          {/* Ruta para manejar 404 Not Found */}
-          <Route path="*" element={<div>404 Not Found</div>} />
-        </Routes>
-        <Footer />
-      </div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Parking" element={<Parking />} />
+        <Route path="/Rent" element={<Rent />} />
+        <Route path="Error404" element={<Error404 />} />
+      </Routes>
+      <Footer />
     </Router>
   );
 };
